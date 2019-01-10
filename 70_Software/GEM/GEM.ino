148911650243e6830dd516efd3c68280d9c00cb1
Luca Schäfli <b7schal@swisslog.com>
2019-01-10 07:26:52 +0100

    Software update

--- 70_Software/GEM/GEM.ino
+++ 70_Software/GEM/GEM.ino
@@ -58,7 +58,8 @@ int iMaxHeightGlass = 100;
 void setup() 
 {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("Hi");
 }
 
 /*******************************************/
